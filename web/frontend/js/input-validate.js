21755fc812f70e6074317d918c54d77cf004bc67
Phuoc149 <67551583+Phuoc149@users.noreply.github.com>
2020-08-07 18:23:27 +0700

    Add files via upload

--- web/frontend/js/input-validate.js
+++ web/frontend/js/input-validate.js
@@ -88,3 +88,205 @@ function checkPass(){
         }
     }
 }
+function checkName () {
+    var name = document.getElementById("name-input")
+    var school = document.getElementById("school-input")
+
+    var blankName = document.getElementById("blank-name")
+    var checkSymbol = document.getElementById("check-symbol")
+    var blankName2 = document.getElementById("blank-name2");
+    var checkSymbol2 = document.getElementById("check-symbol2");
+
+    //when click field, show message box
+    name.onfocus = function() {
+        document.getElementById("name-validate").style.display = "block";
+    }
+    // When click outside field, hide the message box
+    name.onblur = function() {
+        document.getElementById("name-validate").style.display = "none";
+    }
+    name.onkeyup = function () {
+        if(name.value == ""){
+            blankName.classList.remove("valid");
+            blankName.classList.add("invalid");
+        }else{
+            blankName.classList.remove("invalid");
+            blankName.classList.add("valid");
+        }
+        var s = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/
+        if(name.value.match(s)){
+            checkSymbol.classList.remove("valid");
+            checkSymbol.classList.add("invalid");
+        }
+        else {
+            checkSymbol.classList.remove("invalid");
+            checkSymbol.classList.add("valid");
+        }
+    }
+    //when click field, show message box
+    school.onfocus = function() {
+        document.getElementById("school-validate").style.display = "block";
+    }
+    // When click outside field, hide the message box
+    school.onblur = function() {
+        document.getElementById("school-validate").style.display = "none";
+    }
+
+    school.onkeyup=function () {
+        if(school.value == ""){
+            blankName2.classList.remove("valid");
+            blankName2.classList.add("invalid");
+        }else{
+            blankName2.classList.remove("invalid");
+            blankName2.classList.add("valid");
+        }
+        var s = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/
+        if(school.value.match(s)){
+            checkSymbol2.classList.remove("valid");
+            checkSymbol2.classList.add("invalid");
+        }
+        else {
+            checkSymbol2.classList.remove("invalid");
+            checkSymbol2.classList.add("valid");
+        }
+    }
+
+
+}
+//------------------------------------------------------------------------------
+function checkPhone() {
+    var phone = document.getElementById("phone-input")
+    var blankPhone = document.getElementById("blank-phone");
+    var checkPhone = document.getElementById("check-phone")
+    //when click field, show message box
+    phone.onfocus = function () {
+        document.getElementById("phone-validate").style.display = "block";
+    }
+    // When click outside field, hide the message box
+    phone.onblur = function () {
+        document.getElementById("phone-validate").style.display = "none";
+    }
+
+    phone.onkeyup = function () {
+        if (phone.value == "") {
+            blankPhone.classList.remove("valid");
+            blankPhone.classList.add("invalid");
+        } else {
+            blankPhone.classList.remove("invalid");
+            blankPhone.classList.add("valid");
+        }
+        var number = /[0-9]/g;
+        if (phone.value.match(number)) {
+            checkPhone.classList.remove("invalid");
+            checkPhone.classList.add("valid");
+        } else {
+            checkPhone.classList.remove("valid");
+            checkPhone.classList.add("invalid");
+        }
+    }
+}
+//---------------------------------------------------------------------------------------------------
+function checkEmail() {
+    var mail = document.getElementById("mail-input");
+    var blankMail = document.getElementById("blank-mail");
+    var checkMail = document.getElementById("check-mail");
+    var symbolCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    // When the user clicks on the email field, show the message box
+    mail.onfocus = function() {
+        document.getElementById("email-validate").style.display = "block";
+    }
+    // When the user clicks outside of the email field, hide the message box
+    mail.onblur = function() {
+        document.getElementById("email-validate").style.display = "none";
+    }
+    mail.onkeyup = function() {
+        if (mail.value == ""){
+            blankMail.classList.remove("valid");
+            blankMail.classList.add("invalid");
+        }else {
+            blankMail.classList.remove("invalid");
+            blankMail.classList.add("valid")
+        }
+        if(symbolCheck.test(mail.value)) {
+            checkMail.classList.remove("invalid");
+            checkMail.classList.add("valid");
+        } else {
+            checkMail.classList.remove("valid");
+            checkMail.classList.add("invalid");
+        }
+
+        }
+}
+//--------------------------------------------------------------------------------------------------
+function buttonRegister() {
+    var name = document.getElementById("name-input")
+    var school = document.getElementById("school-input")
+    var phone = document.getElementById("phone-input")
+    var mail = document.getElementById("mail-input");
+    if(name.value && school.value && phone.value && mail.value !=""  ){
+        alert("Nhận mã xác nhận qua email vừa đăng ký")
+        location.href="http://localhost:8080/LearnMath/frontend/html/Verification.jsp"
+    }else {
+        alert("Kiểm tra lại thông tin")
+    }
+
+}
+//---------------------------------------------------------------------------------------------------
+//trang ma xac nhan
+function checkVerification() {
+        var veriPass = document.getElementById("verification-input")
+
+        if (veriPass.value == "") {
+            alert("Chưa nhập mã xác nhận")
+        } else {
+            location.href = "http://localhost:8080/LearnMath/frontend/html/ChangePassword.jsp"
+            //http://localhost:8080/LearnMath/frontend/html/Verification.jsp
+        }
+    }
+    //-----------------------------------------------------------------------------------------------
+// trang doi mat khau
+    function checkRePass() {
+        var pass = document.getElementById("pass-input")
+        var rePass = document.getElementById("repass-input")
+        var matchPass = document.getElementById("matchPass")
+        // When the user clicks on the password field, show the message box
+        rePass.onfocus = function () {
+            document.getElementById("repass-validate").style.display = "block";
+        }
+        // When the user clicks outside of the password field, hide the message box
+        rePass.onblur = function () {
+            document.getElementById("repass-validate").style.display = "none";
+        }
+        rePass.onkeyup = function () {
+
+            if (pass.value != rePass.value) {
+                matchPass.classList.remove("valid");
+                matchPass.classList.add("invalid");
+            } else {
+                matchPass.classList.remove("invalid");
+                matchPass.classList.add("valid");
+            }
+        }
+    }
+
+//funcition button trang thay đỏi mat khau
+//funcition nut đk
+    function nonfitication() {
+        var pass = document.getElementById("pass-input")
+        var rePass = document.getElementById("repass-input")
+        if (rePass.value == "") {
+            alert("Chưa nhập mật khẩu")
+        }
+        if (pass.value != rePass.value) {
+            alert("Mật khẩu không trùng khớp")
+        }
+        if (pass.value == rePass.value) {
+            alert("Hoàn thành đăng ký ")
+        }
+    }
+
+//funcition nút hủy
+    function cancelNonfitication() {
+        alert("Xác nhận hủy đăng ký")
+    }
